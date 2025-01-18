e8f61048cbad977cd1dcb5f9c5a5a8ebf4b0ede9
Dani71113 <danielwelde867@gmail.com>
2025-01-18 15:17:53 +0300

    out put structure changes

--- main.cpp
+++ main.cpp
@@ -13,7 +13,9 @@ struct Item {
 // Function to get user input for number of items
 int getNumItems() {
     int numItems;
-    cout << "Enter the number of items: ";
+    cout << "Welcome to the Knapsack Optimization Program!" << endl;
+    cout << "This program helps you maximize the total value of items you can carry in a knapsack with limited capacity." << endl;
+    cout << "Enter the number of items you want to evaluate: ";
     cin >> numItems;
     return numItems;
 }
@@ -21,11 +23,12 @@ int getNumItems() {
 // Function to get user input for an item
 Item getItem(int itemNumber) {
     Item item;
-    cout << "Enter name for item " << itemNumber << ": ";
+    cout << "Enter details for item " << itemNumber << ":" << endl;
+    cout << "  Name: ";
     cin >> item.name;
-    cout << "Enter weight for item " << itemNumber << ": ";
+    cout << "  Weight: ";
     cin >> item.weight;
-    cout << "Enter value for item " << itemNumber << ": ";
+    cout << "  Value: ";
     cin >> item.value;
     return item;
 }
@@ -34,7 +37,14 @@ Item getItem(int itemNumber) {
 vector<Item> getItems(int numItems) {
     vector<Item> items;
     for (int i = 0; i < numItems; ++i) {
-        items.push_back(getItem(i + 1)); 
+        items.push_back(getItem(i + 1));
+    }
+    cout << "\nYou have entered the following items:" << endl;
+    for (int i = 0; i < numItems; ++i) {
+        cout << "  Item " << i + 1 << ": " 
+             << items[i].name << " (Weight: " 
+             << items[i].weight << ", Value: " 
+             << items[i].value << ")" << endl;
     }
     return items;
 }
@@ -42,40 +52,69 @@ vector<Item> getItems(int numItems) {
 // Function to get knapsack capacity
 int getCapacity() {
     int capacity;
-    cout << "Enter the knapsack capacity: ";
+    cout << "\nEnter the total capacity of the knapsack: ";
     cin >> capacity;
     return capacity;
 }
 
 // Knapsack algorithm (0/1 Knapsack)
-int knapsack(vector<Item>& items, int capacity) {
+pair<int, vector<int>> knapsack(vector<Item>& items, int capacity) {
     int n = items.size();
     vector<vector<int>> dp(n + 1, vector<int>(capacity + 1, 0));
+    vector<vector<bool>> keep(n + 1, vector<bool>(capacity + 1, false));
 
     for (int i = 1; i <= n; ++i) {
         for (int w = 1; w <= capacity; ++w) {
             if (items[i - 1].weight <= w) {
-                dp[i][w] = max(dp[i - 1][w], items[i - 1].value + dp[i - 1][w - items[i - 1].weight]);
+                if (items[i - 1].value + dp[i - 1][w - items[i - 1].weight] > dp[i - 1][w]) {
+                    dp[i][w] = items[i - 1].value + dp[i - 1][w - items[i - 1].weight];
+                    keep[i][w] = true;
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
             } else {
                 dp[i][w] = dp[i - 1][w];
             }
         }
     }
 
-    return dp[n][capacity];
+    vector<int> selectedItems;
+    int w = capacity;
+    for (int i = n; i > 0; --i) {
+        if (keep[i][w]) {
+            selectedItems.push_back(i - 1); // Store index of the item
+            w -= items[i - 1].weight;
+        }
+    }
+
+    return {dp[n][capacity], selectedItems};
 }
 
 // Function to display the result
-void displayResult(int maxValue) {
+void displayResult(int maxValue, const vector<Item>& items, const vector<int>& selectedItems) {
+    cout << "\n=== Knapsack Optimization Result ===" << endl;
     cout << "Maximum value that can be obtained: " << maxValue << endl;
+    if (selectedItems.empty()) {
+        cout << "No items could be selected within the given capacity." << endl;
+    } else {
+        cout << "Selected items are:" << endl;
+        for (int idx : selectedItems) {
+            cout << " - " << items[idx].name 
+                 << " (Weight: " << items[idx].weight 
+                 << ", Value: " << items[idx].value << ")" << endl;
+        }
+    }
+    cout << "\nThank you for using the Knapsack Optimization Program!" << endl;
 }
 
 int main() {
     int numItems = getNumItems();
     vector<Item> items = getItems(numItems);
     int capacity = getCapacity();
-    int maxValue = knapsack(items, capacity);
-    displayResult(maxValue);
+
+    auto [maxValue, selectedItems] = knapsack(items, capacity);
+
+    displayResult(maxValue, items, selectedItems);
 
     return 0;
 }
